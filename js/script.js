630c8bc6262b52a999e5ea25073ccf8cc2ad624b
Hnatkiv <gnatkiv@icloud.com>
2022-09-30 13:31:36 +0300

    second comid work with if

--- js/script.js
+++ js/script.js
@@ -10,14 +10,40 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один із останніх фільмів?', ''),
+/* const a = prompt('Один із останніх фільмів?', ''),
       b = prompt('Яку оцінку дасте йому?', ''),
       c = prompt('Один із останніх фільмів?', ''),
       d = prompt('Яку оцінку дасте йому?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[c] = d; */    //цю частину ми автоматизували часимною нижче
+
+for ( let i = 0; i < 2; i++) {
+    const a = prompt('Один із останніх фільмів?', ''),
+          b = prompt('Яку оцінку дасте йому?', '');
+
+    if (a != null  && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Ви подивились мало фільмів =(');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Ви звичайний глядач');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ви кіноман');
+} else {
+    console.log('Помилка');
+}
+
+
 
 console.log(personalMovieDB);
+
 
 
